e3a4ceafff89193b0538ee420dfe978dc978b50d
Fuexfollets <fuex9follets@gmail.com>
2025-04-29 13:12:08 -0400

    Partial implementation for tensorflow model v1

--- holographic_forecast/data/data_encoding.py
+++ holographic_forecast/data/data_encoding.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 import holographic_forecast.data.data_models as data_models
 
 
-class WeatherModelV1Encoder:
+class WeatherEncoderV1:
     """
     WeatherModelV1Encoder
         - Output Shape: (n timesteps (ragged), 1 + n points (ragged), 1 + n entries (ragged) | 1, 1 + n identifier chars (ragged) | 2 + n predicted identifier chars (ragged))
@@ -29,10 +29,12 @@ class WeatherModelV1Encoder:
         weather_entry: data_models.WeatherEntry,
     ) -> list[float | int]:
         """
-        Output Shape: (1 + n identifier chars (ragged))
+        Output Shape: (
+                value (shape () at [0]) + identifier chars (ragged)
+                )
         """
 
-        return [weather_entry.value] + WeatherModelV1Encoder.encode_weather_quantity(
+        return [weather_entry.value] + WeatherEncoderV1.encode_weather_quantity(
             weather_entry.quantity
         )
 
@@ -41,7 +43,10 @@ class WeatherModelV1Encoder:
         weather_time_point: data_models.WeatherTimePoint,
     ) -> list[list[float | int]]:
         """
-        Output Shape: (1 + n entries (ragged), 1 + n identifier chars (ragged) | 3)
+        Output Shape: (
+                point info (shape (3,) at [0]) + n entries (ragged)
+                value (shape () at [0]) + identifier chars (ragged)
+                )
         """
 
         return [
@@ -51,7 +56,7 @@ class WeatherModelV1Encoder:
                 weather_time_point.time.timestamp(),
             ],
             *(
-                WeatherModelV1Encoder.encode_weather_entry(entry)
+                WeatherEncoderV1.encode_weather_entry(entry)
                 for entry in weather_time_point.data
             ),
         ]
@@ -63,17 +68,21 @@ class WeatherModelV1Encoder:
         predicted_quantity: data_models.WeatherQuantity,
     ) -> list[list[list[float | int]]]:
         """
-        Output Shape: (1 + n points (ragged), 1 + n entries (ragged) | 1, 1 + n identifier chars (ragged) | 2 + n predicted identifier chars (ragged))
+        Output Shape: (
+                query info (shape (2 + n identifier chars (ragged)) at [0][0]) + n points (ragged),
+                point info (shape (3,) at [0]) + n entries (ragged)
+                value (shape () at [0]) + identifier chars (ragged)
+                )
         """
 
         encoded_query: list[float | int] = [  # Ragged
             predicted_cordinate.latitude_deg,
             predicted_cordinate.longitude_deg,
-            *WeatherModelV1Encoder.encode_weather_quantity(predicted_quantity),
+            *WeatherEncoderV1.encode_weather_quantity(predicted_quantity),
         ]
 
         return [[encoded_query]] + [
-            WeatherModelV1Encoder.encode_weather_time_point(weather_time_point)
+            WeatherEncoderV1.encode_weather_time_point(weather_time_point)
             for weather_time_point in weather_time_area.data
         ]
 
@@ -84,11 +93,16 @@ class WeatherModelV1Encoder:
         predicted_quantity: data_models.WeatherQuantity,
     ) -> list[list[list[list[float | int]]]]:
         """
-        Output Shape: (n timesteps (ragged), 1 + n points (ragged), 1 + n entries (ragged) | 1, 1 + n identifier chars (ragged) | 2 + n predicted identifier chars (ragged))
+        Output Shape: (
+                n timesteps (ragged),
+                query info (shape (2 + n identifier chars (ragged)) at [0][0]) + n points (ragged),
+                point info (shape (3,) at [0]) + n entries (ragged)
+                value (shape () at [0]) + identifier chars (ragged)
+                )
         """
 
         return [
-            WeatherModelV1Encoder.encode_weather_time_area(
+            WeatherEncoderV1.encode_weather_time_area(
                 weather_time_area, predicted_cordinate, predicted_quantity
             )
             for weather_time_area in weather_timespan_area.data.values()
@@ -100,12 +114,20 @@ class WeatherModelV1Encoder:
         predicted_cordinate: data_models.GeographicCordinate,
         predicted_quantity: data_models.WeatherQuantity,
     ) -> tf.Tensor:
+        """
+        Output Shape: (
+                n timesteps (ragged),
+                query info (shape (2 + n identifier chars (ragged)) at [0][0]) + n points (ragged),
+                point info (shape (3,) at [0]) + n entries (ragged)
+                value (shape () at [0]) + identifier chars (ragged)
+                )
+        """
         return cast(
             tf.Tensor,
             cast(
                 tf.RaggedTensor,
                 tf.ragged.constant(
-                    WeatherModelV1Encoder.encode_weather_timespan_area(
+                    WeatherEncoderV1.encode_weather_timespan_area(
                         weather_timespan_area, predicted_cordinate, predicted_quantity
                     )
                 ),
--- holographic_forecast/weather_models_tf.py
+++ holographic_forecast/weather_models_tf.py
@@ -1,12 +1,39 @@
 # pyright: reportUnknownMemberType=false
 # pyright: reportMissingTypeStubs=false
 
-from typing import override, cast
+from typing import cast
 
 import tensorflow as tf
+import tensorflow.python.framework.ops as tf_ops
 import keras
 
 
 @keras.saving.register_keras_serializable()
 class WeatherModelV1(keras.Model):
+    """
+    Encoder: Uses WeatherEncoderV1
 
+    Input Shape: (
+            n timesteps (ragged),
+            query info (shape (2 + n identifier chars (ragged)) at [0][0]) + n points (ragged),
+            point info (shape (3,) at [0]) + n entries (ragged),
+            value (shape () at [0]) + identifier chars (ragged)
+            )
+
+    Output shape: (
+            value (shape ())
+            )
+    """
+
+    weather_char_identifier_embedding: keras.layers.Embedding
+
+    def __init__(self):
+        super().__init__()
+
+        self.weather_char_identifier_embedding = keras.layers.Embedding(
+            input_dim=1, output_dim=16
+        )
+
+    def call(self, query: tf.Tensor) -> tf.Tensor:
+        # Shape: (timesteps, points, entries, identifier chars)
+        entry_identifier_chars: tf.Tensor = cast(tf.Tensor, query)[:, 1:, :, 1:]
