e50d9a4db47e01463fb8820d37beffdb5cc64ac9
Fuexfollets <fuex9follets@gmail.com>
2025-05-07 15:01:48 -0400

    Implemented save/load mechanic for weather forecaster

---
+++ holographic_forecast/weather_forecaster_torch.py
@@ -0,0 +1,49 @@
+# pyright: reportAny=false
+
+import pathlib
+import pickle
+from dataclasses import dataclass
+
+import torch
+
+import holographic_forecast.data.data_models as data_models
+import holographic_forecast.data.data_encoding_torch as data_encoding
+import holographic_forecast.weather_models_torch as torch_models
+
+
+@dataclass
+class WeatherForecasterTorch:
+    weather_timespan_area: data_models.WeatherTimespanArea
+    weather_model: torch_models.WeatherModelV1
+
+    progress_path: pathlib.Path | None = None
+    loaded: bool = False
+
+    def load(self, path: pathlib.Path | None = None):
+        if path is None:
+            path = self.progress_path
+
+        if path is None:
+            raise ValueError(
+                "No path provided in either self.save_path or argument path"
+            )
+
+        with path.open("rb") as file:
+            self.loaded = True
+            self.progress_path = path
+            self.weather_timespan_area = pickle.load(file)
+            self.weather_model = torch_models.WeatherModelV1()
+            _ = self.weather_model.load_state_dict(torch.load(path.with_suffix(".pt")))
+
+    def save(self, path: pathlib.Path | None = None):
+        if path is None:
+            path = self.progress_path
+
+        if path is None:
+            raise ValueError(
+                "No path provided in either self.save_path or argument path"
+            )
+
+        with path.open("wb") as file:
+            pickle.dump(self.weather_timespan_area, file)
+            torch.save(self.weather_model.state_dict(), path.with_suffix(".pt"))
